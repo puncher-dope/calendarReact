824b3a8081ba6f98ac263e8201906cd14842a807
Kostya <kimkonstantin2118@gmail.com>
2025-04-03 20:43:25 +0300

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -29,9 +29,6 @@ export default function App() {
       <h1>Hello world!!</h1>
       <button onClick={onClose}>Закрыть</button>
       {/* <CalendarGfg/> */}
-      <br />
-      <br />
-      <a href="https://www.appsheet.com/start/964ff124-11fb-4408-b51e-2978117a4f7a?platform=desktop#vss=H4sIAAAAAAAAA6WRTYoCMRCFryK1bjxAlv4wDKKLUdx0ZIh2NYSJnaY78YcmMAeYxey9hAizGtArxBtZ8YdxMRvtXVLF93jvVQULicuhEbMPYHH19-vhGhhUHEbrHDkwDm2dmUIrDhGHgZhfhn7rD8dP_-N_j18cHLionsZ7B42Q6iw1iW5SBktg1eNuWP1AEcgEMyNTiUXQCzTpXFlaB5IG9xy1AHNrxFTh2TxxjkbPFlEzxZ3SM2Gu-D-Z4E0vL378hogdEXu_a_jvJim9FNrmLVrGdMahLszt3cFUWGXGQtlw1XjiQjepntkSkzHlrJevfM26q1xkSV8nZDIVqkR3Ah4hKuLmAgAA&row=Лавров%20А.&view=апрель_Detail&appName=КопияГрафикПуриАзов-641903456">Link</a>
     </>
   )
 }
