f199fa65755531561dd8649af8ba14e927b666ca
Kostya <kimkonstantin2118@gmail.com>
2025-04-18 00:54:31 +0300

    ec

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,5 @@ window.location.replace('https://www.appsheet.com/start/964ff124-11fb-4408-b51e-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
